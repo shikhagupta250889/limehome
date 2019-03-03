36ec61a4b8a6f27d57548b5f9dd624308c2aa643
Shikha Gupta <shikhagupta.250889@gmail.com>
2019-03-04 00:03:59 +0530

    Just the basic app structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker'
+
+registerServiceWorker();
